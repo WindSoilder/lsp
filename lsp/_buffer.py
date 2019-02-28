89e37aa58908d6ac5238f1a36d36f1223651c4b9
WindSoilder <WindSoilder@outlook.com>
2019-02-28 18:16:22 +0800

    add FixedLengthBuffer.

---
+++ lsp/_buffer.py
@@ -0,0 +1,43 @@
+class FixedLengthBuffer:
+    def __init__(self):  # type: ignore
+        self.length: int = 0
+        self.data = bytearray()
+        self.length_set = False
+
+    def append(self, data: bytes) -> None:
+        """ append data into buffer.
+
+        Args:
+            data (bytes): data we need to append to.
+        Raises:
+            RuntimeError - When the length of data is more than the buffer capacity.
+        """
+        checked_length = self.length - len(data)
+        if checked_length < 0:
+            raise RuntimeError("Too much data to insert into buffer.")
+        self.length -= len(data)
+        self.data.extend(data)
+
+    def set_length(self, length: int) -> None:
+        """ set the length of buffer.  Note that if the length is set, we can't call
+        `set_length` without calling `clear` method.
+
+
+        Args:
+            length (int): the length of buffer
+        Raises:
+            RuntimeError - When the length is already set
+        """
+        if self.length_set:
+            raise RuntimeError(
+                "You have already call set_length.  This can't be called twice"
+                "If you wan't to reset length, please call `clear()` first"
+            )
+        self.length = length
+        self.length_set = True
+
+    def clear(self) -> None:
+        """ clear the buffer. """
+        self.length_set = False
+        self.data.clear()
+        self.length = 0
---
+++ lsp/tests/test_buffer.py
@@ -0,0 +1,70 @@
+import pytest
+from .._buffer import FixedLengthBuffer
+
+
+def test_buffer_init_state():
+    buffer = FixedLengthBuffer()
+    assert buffer.length == 0
+    assert buffer.data == b""
+    assert buffer.length_set is False
+
+
+def test_buffer_set_length():
+    buffer = FixedLengthBuffer()
+    buffer.set_length(30)
+
+    assert buffer.length == 30
+    assert buffer.length_set is True
+
+
+def test_buffer_set_length_twice_should_raise_error():
+    buffer = FixedLengthBuffer()
+    buffer.set_length(30)
+
+    with pytest.raises(RuntimeError):
+        buffer.set_length(30)
+
+
+def test_buffer_append():
+    buffer = FixedLengthBuffer()
+    buffer.set_length(30)
+    buffer.append(b"456")
+
+    assert (
+        buffer.length == 27
+    ), "After appending data, the length of buffer should be less"
+    assert (
+        buffer.data == b"456"
+    ), "After appending data, there should have data in buffer"
+
+    buffer.append(b"789")
+    assert (
+        buffer.length == 24
+    ), "After appending data, the length of buffer should be less"
+    assert (
+        buffer.data == b"456789"
+    ), "After appending data, there should have data in buffer"
+
+    buffer.append(b"x" * 24)
+    assert buffer.length == 0
+    assert buffer.data == b"456" + b"789" + b"x" * 24
+
+
+def test_buffer_append_too_much_data():
+    buffer = FixedLengthBuffer()
+    buffer.set_length(2)
+    with pytest.raises(RuntimeError):
+        buffer.append(b"x" * 3)
+
+
+def test_clear():
+    buffer = FixedLengthBuffer()
+    buffer.set_length(2)
+
+    buffer.clear()
+    assert buffer.length == 0
+    assert buffer.data == b""
+    assert buffer.length_set is False
+
+    # test we can call clear many twice without throw any error.
+    buffer.clear()
