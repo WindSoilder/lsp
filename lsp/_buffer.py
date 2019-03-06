e0b261a3bf727e416105eeb75b30c30acdc664c1
WindSoilder <WindSoilder@outlook.com>
2019-03-06 18:50:09 +0800

    re-implement ReceiveBuffer.

--- lsp/_buffer.py
+++ lsp/_buffer.py
@@ -1,4 +1,4 @@
-from typing import Optional, Union
+from typing import Optional, Dict
 
 
 class FixedLengthBuffer:
@@ -60,6 +60,8 @@ class ReceiveBuffer:
             buffer = FixedLengthBuffer()
         self._inner_buffer = buffer
         self.raw = bytearray()
+        self.header_bytes: Optional[bytearray] = None
+        self.body_pointer: int = 0
 
     def append(self, data: bytes) -> None:
         """ Append data into buffer.
@@ -68,3 +70,57 @@ class ReceiveBuffer:
             data (bytes): the data we need to append.
         """
         self.raw.extend(data)
+
+    def try_extract_header(self) -> Optional[Dict[str, str]]:
+        """ Try to extract the header part in the buffer.
+
+        Returns:
+            When the buffer received completely header data, then return
+            A dict.  Else we return None.
+        """
+
+        def _extract_header() -> Dict[str, str]:
+            header_str = self.header_bytes.decode("ascii")  # type: ignore
+            results = {}
+            rows = header_str.split("\r\n")
+            for row in rows:
+                key, val = row.split(": ")
+                results[key] = val
+            return results
+
+        data_splitted = self.raw.split(b"\r\n\r\n")
+        if len(data_splitted) == 1:  # so we don't receive header data complete yet.
+            return None
+        else:
+            # we have receive header completely, so we can extract header, and if there
+            # are any data inputed, we save it to the raw, which indicate that it's
+            # un-handled.
+            self.header_bytes, self.raw = (
+                bytearray(data_splitted[0]),
+                bytearray(data_splitted[1]),
+            )
+            headers = _extract_header()
+            return headers
+
+    def try_extract_data(self) -> Optional[bytes]:
+        """ Try to extract the actual data in buffer.  Note that we should call
+        `try_extract_header` first to extract header out.
+
+        Returns:
+            When there are data in the buffer, return it.  Return None to indicate
+            there are no data in the buffer.
+
+        Raises:
+            RuntimeError - When the buffer doesn't completely handle header data.
+        """
+        if self.header_bytes is None:
+            raise RuntimeError(
+                "Header is un-handled yet, please call `try_extract_header` first."
+            )
+        # TODO: need to rewrite the implementation.  Because the slice operation will
+        # copy memeory, and it may be high cost.
+        # fmt: off
+        data = self.raw[self.body_pointer:]
+        # fmt: on
+        self.body_pointer += len(data)
+        return data
--- lsp/tests/test_buffer.py
+++ lsp/tests/test_buffer.py
@@ -92,3 +92,51 @@ def test_receive_buffer_append():
 
     buffer.append(b"ghjk")
     assert buffer.raw == b"asdfghjk"
+
+
+def test_receive_buffer_try_extract_header():
+    buffer = ReceiveBuffer()
+    buffer.append(b"Content-Length: 123\r\n\r\n")
+    header = buffer.try_extract_header()
+    assert header == {"Content-Length": "123"}
+
+    # for now the header is incomplete
+    buffer = ReceiveBuffer()
+    buffer.append(b"Content-Length: 123\r\n\r")
+    header = buffer.try_extract_header()
+    assert header is None
+    # then we complete the header data
+    buffer.append(b"\n")
+    header = buffer.try_extract_header()
+    assert header == {"Content-Length": "123"}
+
+
+def test_receive_buffer_try_extract_data():
+    buffer = ReceiveBuffer()
+    buffer.append(b"Content-Length: 123\r\n\r\n")
+    buffer.try_extract_header()
+
+    buffer.append(b"first data")
+    data = buffer.try_extract_data()
+    assert data == b"first data"
+    # when extract data out, we should extract out emtpy data.
+    data = buffer.try_extract_data()
+    assert data == b""
+    buffer.append(b"second data")
+    data = buffer.try_extract_data()
+    assert data == b"second data"
+
+
+def test_receive_buffer_try_extract_data_when_header_is_unhandled_yet():
+    buffer = ReceiveBuffer()
+    buffer.append(b"Content-Length:123\r\n\r\n")
+    with pytest.raises(RuntimeError):
+        buffer.try_extract_data()
+
+
+def test_receive_buffer_try_extract_data_when_no_data_yet():
+    buffer = ReceiveBuffer()
+    buffer.append(b"Content-Length: 123\r\n\r\n")
+    buffer.try_extract_header()
+    data = buffer.try_extract_data()
+    assert data == b""
